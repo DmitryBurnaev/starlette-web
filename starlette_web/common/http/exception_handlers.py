bde6aa53e3fdba2ddae511da8be8f24eb54dd7f8
Sergey Sayamov <dolamroth@mail.ru>
2022-05-03 23:48:00 +0300

    Further decompose base exception handler class

--- starlette_web/common/http/exception_handlers.py
+++ starlette_web/common/http/exception_handlers.py
@@ -1,7 +1,9 @@
 import logging
 import logging.config
+from typing import Any, Optional
 
 from starlette import status
+from starlette.responses import BackgroundTask
 from webargs_starlette import WebargsHTTPException
 
 from starlette_web.core import settings
@@ -13,42 +15,64 @@ from starlette_web.common.utils import log_message
 
 
 class BaseExceptionHandler:
-    def _get_error_message(self, request: PRequest, exc: Exception):
+    renderer_class: BaseRenderer = JSONRenderer
+
+    def _get_error_message(self, request: PRequest, exc: Exception) -> str:
         return "Something went wrong!"
 
-    def _get_error_details(self, request: PRequest, exc: Exception):
+    def _get_error_details(self, request: PRequest, exc: Exception) -> str:
         return f"Raised Error: {exc.__class__.__name__}"
 
-    def _get_status_code(self, request: PRequest, exc: Exception):
+    def _get_status_code(self, request: PRequest, exc: Exception) -> int:
         return getattr(exc, "status_code", status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def _get_response_status(self, request: PRequest, exc: Exception):
+    def _get_response_status(self, request: PRequest, exc: Exception) -> str:
         return ResponseStatus.INTERNAL_ERROR
 
-    def __call__(self, request: PRequest, exc: Exception) -> BaseRenderer:
+    def _get_response_data(self, request: PRequest, exc: Exception) -> Any:
         error_message = self._get_error_message(request, exc)
         error_details = self._get_error_details(request, exc)
-        status_code = self._get_status_code(request, exc)
         response_status = self._get_response_status(request, exc)
 
         payload = {"error": error_message}
         if settings.APP_DEBUG or response_status == ResponseStatus.INVALID_PARAMETERS:
             payload["details"] = error_details
 
-        response_data = {"status": response_status, "payload": payload}
+        return {"status": response_status, "payload": payload}
+
+    def _on_error_action(self, request: PRequest, exc: Exception):
+        status_code = self._get_status_code(request, exc)
+        error_message = self._get_error_message(request, exc)
+        payload = {"error": error_message}
+
         log_level = logging.ERROR if status_is_server_error(status_code) else logging.WARNING
-        log_message(exc, response_data["payload"], log_level)
-        return JSONRenderer(response_data, status_code=status_code)
+        log_message(exc, payload, log_level)
+
+    def _get_headers(self, request: PRequest, exc: Exception) -> Optional[dict]:
+        return None
+
+    def _get_background_tasks(self, request: PRequest, exc: Exception) -> Optional[BackgroundTask]:
+        return None
+
+    def __call__(self, request: PRequest, exc: Exception) -> BaseRenderer:
+        self._on_error_action(request, exc)
+
+        return self.renderer_class(
+            content=self._get_response_data(request, exc),
+            status_code=self._get_status_code(request, exc),
+            background=self._get_background_tasks(request, exc),
+            headers=self._get_headers(request, exc),
+        )
 
 
 class BaseApplicationErrorHandler(BaseExceptionHandler):
-    def _get_error_details(self, request: PRequest, exc: BaseApplicationError):
+    def _get_error_details(self, request: PRequest, exc: BaseApplicationError) -> str:
         return exc.details
 
-    def _get_error_message(self, request: PRequest, exc: BaseApplicationError):
+    def _get_error_message(self, request: PRequest, exc: BaseApplicationError) -> str:
         return exc.message
 
-    def _get_response_status(self, request: PRequest, exc: BaseApplicationError):
+    def _get_response_status(self, request: PRequest, exc: BaseApplicationError) -> str:
         return exc.response_status
 
 
@@ -56,11 +80,11 @@ class WebargsHTTPExceptionHandler(BaseExceptionHandler):
     def _get_error_details(self, request: PRequest, exc: WebargsHTTPException):
         return exc.messages.get("json") or exc.messages.get("form") or exc.messages
 
-    def _get_error_message(self, request: PRequest, exc: WebargsHTTPException):
+    def _get_error_message(self, request: PRequest, exc: WebargsHTTPException) -> str:
         return "Requested data is not valid."
 
-    def _get_response_status(self, request: PRequest, exc: WebargsHTTPException):
+    def _get_response_status(self, request: PRequest, exc: WebargsHTTPException) -> str:
         return ResponseStatus.INVALID_PARAMETERS
 
-    def _get_status_code(self, request: PRequest, exc: Exception):
+    def _get_status_code(self, request: PRequest, exc: Exception) -> int:
         return status.HTTP_400_BAD_REQUEST
