ca8d615dd2f091221cb8402e53ad8233ebbf5c5f
Sergey Sayamov <dolamroth@mail.ru>
2023-02-16 23:12:54 +0300

    Dynamically obtain url for endpoint of apispec openapi schema

--- starlette_web/contrib/apispec/routes.py
+++ starlette_web/contrib/apispec/routes.py
@@ -4,5 +4,5 @@ from starlette_web.contrib.apispec import views
 
 routes = [
     Route("/redoc/", views.RedocView, include_in_schema=False),
-    Route("/schema/", views.OpenApiView, include_in_schema=False),
+    Route("/schema/", views.OpenApiView, include_in_schema=False, name="apispec_openapi_schema"),
 ]
