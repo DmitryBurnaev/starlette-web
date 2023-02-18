6a50433601ca016a7232304b21e4fddacf331dec
Sergey Sayamov <dolamroth@mail.ru>
2023-02-18 23:33:46 +0300

    Fix exception handling in BaseConstanceBackend

--- starlette_web/contrib/constance/backends/base.py
+++ starlette_web/contrib/constance/backends/base.py
@@ -1,8 +1,11 @@
-import pickle
 from typing import List, Dict, Any, Type
 
 from starlette_web.common.http.exceptions import NotSupportedError, UnexpectedError
-from starlette_web.common.utils.serializers import BaseSerializer, PickleSerializer
+from starlette_web.common.utils.serializers import (
+    BaseSerializer,
+    PickleSerializer,
+    DeserializeError,
+)
 
 
 class BaseConstanceBackend:
@@ -16,12 +19,13 @@ class BaseConstanceBackend:
         if response is None:
             return self.empty
 
+        # TODO: maybe support memoryview ?
         if type(response) != bytes:
             raise NotSupportedError
 
         try:
             return self.serializer.deserialize(response)
-        except pickle.PickleError as exc:
+        except DeserializeError as exc:
             raise UnexpectedError(details=str(exc))
 
     async def mget(self, keys: List[str]) -> Dict[str, Any]:
