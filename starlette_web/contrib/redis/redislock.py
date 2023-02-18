2a8ea45894e74dbf804e0f1360d7e4e22afcccb7
Sergey Sayamov <dolamroth@mail.ru>
2023-02-18 17:37:29 +0300

    Set a fixed deadline for releasing redis lock (in case connection is lost)

--- starlette_web/contrib/redis/redislock.py
+++ starlette_web/contrib/redis/redislock.py
@@ -1,4 +1,4 @@
-import asyncio
+import anyio
 
 from aioredis.exceptions import RedisError
 from aioredis.lock import Lock as AioredisLock
@@ -7,6 +7,8 @@ from starlette_web.common.caches.base import CacheLockError
 
 
 class RedisLock(AioredisLock):
+    EXIT_MAX_DELAY = 10
+
     async def __aenter__(self):
         try:
             return await super().__aenter__()
@@ -20,11 +22,11 @@ class RedisLock(AioredisLock):
             expected_token = self.local.token
             if expected_token is not None:
                 self.local.token = None
-                coroutine = self.lua_release(
-                    keys=[self.name],
-                    args=[expected_token],
-                    client=self.redis,
-                )
-                await asyncio.shield(coroutine)
+                with anyio.move_on_after(self.EXIT_MAX_DELAY, shield=True):
+                    await self.lua_release(
+                        keys=[self.name],
+                        args=[expected_token],
+                        client=self.redis,
+                    )
         except RedisError as exc:
             raise CacheLockError from exc
