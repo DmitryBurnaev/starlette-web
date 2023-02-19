55446c60c91cf51a15871427d0e273d4733e4dfb
Sergey Sayamov <dolamroth@mail.ru>
2023-02-19 17:30:15 +0300

    Introduce timeout to all places, where resourses are cleared at exit

--- starlette_web/common/database/transaction.py
+++ starlette_web/common/database/transaction.py
@@ -1,3 +1,5 @@
+import anyio
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -5,6 +7,7 @@ class AtomicTransaction:
     """
     Asynchronous wrapper around sqlalchemy.Session.begin_nested, that also commits/rollbacks at end
     """
+    EXIT_MAX_DELAY: float = 60
 
     def __init__(self, db_session: AsyncSession, db_commit=True, **kwargs):
         self.db_session = db_session
@@ -19,12 +22,13 @@ class AtomicTransaction:
         return self.nested_block
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self.nested_block_wrapper:
-            if exc_type is not None:
-                await self.nested_block.rollback()
-            elif self.db_commit:
-                await self.nested_block.commit()
+        with anyio.fail_after(self.EXIT_MAX_DELAY, shield=True):
+            if self.nested_block_wrapper:
+                if exc_type is not None:
+                    await self.nested_block.rollback()
+                elif self.db_commit:
+                    await self.nested_block.commit()
 
-            await self.nested_block_wrapper.__aexit__(exc_type, exc_val, exc_tb)
+                await self.nested_block_wrapper.__aexit__(exc_type, exc_val, exc_tb)
 
         return False
--- starlette_web/contrib/redis/redislock.py
+++ starlette_web/contrib/redis/redislock.py
@@ -7,7 +7,7 @@ from starlette_web.common.caches.base import CacheLockError
 
 
 class RedisLock(AioredisLock):
-    EXIT_MAX_DELAY = 10
+    EXIT_MAX_DELAY: float = 60
 
     async def __aenter__(self):
         try:
@@ -22,11 +22,11 @@ class RedisLock(AioredisLock):
             expected_token = self.local.token
             if expected_token is not None:
                 self.local.token = None
-                with anyio.move_on_after(self.EXIT_MAX_DELAY, shield=True):
+                with anyio.fail_after(self.EXIT_MAX_DELAY, shield=True):
                     await self.lua_release(
                         keys=[self.name],
                         args=[expected_token],
                         client=self.redis,
                     )
-        except RedisError as exc:
+        except (RedisError, TimeoutError) as exc:
             raise CacheLockError from exc
