510e34b5fe103026adb8131f8693509a12a3b1fc
Sergey Sayamov <dolamroth@mail.ru>
2023-02-24 11:41:21 +0300

    Refactor contrib.admin

--- starlette_web/contrib/auth/admin.py
+++ starlette_web/contrib/auth/admin.py
@@ -1,9 +1,19 @@
-from starlette_web.contrib.admin import ModelView, admin
-from starlette_web.contrib.auth.models import User
+from starlette_web.contrib.admin import AdminView, admin
+from starlette_web.contrib.auth.models import User, UserSession, UserInvite
 
 
-class UserView(ModelView):
-    fields = []
+class UserView(AdminView):
+    pass
+
+
+class UserSessionView(AdminView):
+    pass
+
+
+class UserInviteView(AdminView):
+    pass
 
 
 admin.add_view(UserView(User, icon="fa fa-users"))
+admin.add_view(UserSessionView(UserSession, icon="fa fa-database"))
+admin.add_view(UserInviteView(UserInvite, icon="fa fa-user-plus"))
