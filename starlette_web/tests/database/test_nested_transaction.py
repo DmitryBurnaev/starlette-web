0ae07df90c7c166f1d547661fbc347ab10211498
Sergey Sayamov <dolamroth@mail.ru>
2023-02-25 20:53:21 +0300

    Deprecated AtomicTransaction wrapper, since it doesn't work as expected, use session.begin_nested directly

--- starlette_web/tests/database/test_nested_transaction.py
+++ starlette_web/tests/database/test_nested_transaction.py
@@ -2,7 +2,6 @@ import uuid
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from starlette_web.common.database.transaction import AtomicTransaction
 from starlette_web.contrib.auth.models import User
 from starlette_web.tests.helpers import await_
 
@@ -47,46 +46,3 @@ def test_nested_transaction(dbs: AsyncSession):
 
         if block1_wrapper and not block_1_exited:
             await_(block1_wrapper.__aexit__(None, None, None))
-
-
-def test_atomic_transaction(dbs: AsyncSession):
-    async def atomic_coroutine(user_obj, db_session):
-        async with AtomicTransaction(db_session=db_session):
-            _ = await User.async_create(db_session=dbs, **user_obj)
-
-        user = await User.async_get(db_session=dbs, **user_obj)
-        return user
-
-    email = str(uuid.uuid4()).replace("-", "") + "@test.com"
-    password = User.make_password(str(uuid.uuid4()))
-    user = await_(atomic_coroutine({"email": email, "password": password}, db_session=dbs))
-    assert user is not None
-
-
-def test_nested_atomic_transactions(dbs: AsyncSession):
-    async def atomic_coroutine(user_obj_1, user_obj_2, db_session):
-        async with AtomicTransaction(db_session=db_session):
-            _ = await User.async_create(db_session=dbs, **user_obj_1)
-
-            try:
-                async with AtomicTransaction(db_session=db_session):
-                    _ = await User.async_create(db_session=dbs, **user_obj_2)
-                    raise Exception("Unexpected error")
-            except:  # noqa: E722
-                pass
-
-        user1 = await User.async_get(db_session=dbs, **user_obj_1)
-        user2 = await User.async_get(db_session=dbs, **user_obj_2)
-        return user1, user2
-
-    email1 = str(uuid.uuid4()).replace("-", "") + "@test.com"
-    password1 = User.make_password(str(uuid.uuid4()))
-    user_obj_1 = {"email": email1, "password": password1}
-
-    email2 = str(uuid.uuid4()).replace("-", "") + "@test.com"
-    password2 = User.make_password(str(uuid.uuid4()))
-    user_obj_2 = {"email": email2, "password": password2}
-
-    user1, user2 = await_(atomic_coroutine(user_obj_1, user_obj_2, db_session=dbs))
-    assert user1 is not None
-    assert user2 is None
