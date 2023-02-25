61156836338a8415b1e2891401ac88d06604a121
Sergey Sayamov <dolamroth@mail.ru>
2023-02-25 22:50:14 +0300

    Minor tweaks to auth.admin for test purposes

--- starlette_web/contrib/auth/admin.py
+++ starlette_web/contrib/auth/admin.py
@@ -1,17 +1,62 @@
+from typing import Any, List, Dict
+
+from starlette.requests import Request
+from starlette_admin.actions import action
+from starlette_admin.exceptions import FormValidationError
+
 from starlette_web.contrib.admin import AdminView, admin
 from starlette_web.contrib.auth.models import User, UserSession, UserInvite
 
 
 class UserView(AdminView):
-    pass
+    exclude_fields_from_edit = ["password"]
+    searchable_fields = ["email"]
+
+    @action(
+        name="delete_cascade",
+        text="Delete cascade",
+        confirmation="Are you sure you want to delete this items?",
+        submit_btn_text="Yes, delete them all",
+        submit_btn_class="btn-danger",
+    )
+    async def delete_cascade_action(self, request: Request, pks: List[Any]) -> str:
+        affected_rows = await self.delete_cascade(request, pks)
+        return "{} items were successfully deleted".format(affected_rows)
+
+    async def validate(self, request: Request, data: Dict[str, Any]) -> None:
+        # TODO: proper email validation
+        errors: Dict[str, str] = {}
+        cleaned_email = data["email"].strip()
+        if not cleaned_email or "@" not in cleaned_email:
+            errors["email"] = "Invalid value for field email."
+        if errors:
+            raise FormValidationError(errors)
 
 
 class UserSessionView(AdminView):
-    pass
+    async def can_create(self, request: Request) -> bool:
+        return False
+
+    async def can_delete(self, request: Request) -> bool:
+        return False
+
+    async def is_action_allowed(self, request: Request, name: str) -> bool:
+        if name == "delete":
+            return await self.can_delete(request)
+        return True
 
 
 class UserInviteView(AdminView):
-    pass
+    async def can_create(self, request: Request) -> bool:
+        return False
+
+    async def can_delete(self, request: Request) -> bool:
+        return False
+
+    async def is_action_allowed(self, request: Request, name: str) -> bool:
+        if name == "delete":
+            return await self.can_delete(request)
+        return True
 
 
 admin.add_view(UserView(User, icon="fa fa-users"))
